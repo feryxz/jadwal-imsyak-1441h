408a84f1e0a4f6ffe95d0bb51ecb83b514606bbf
Feryxz Developers <58109486+feryxz@users.noreply.github.com>
2020-05-23 15:17:18 +0700

    first commit

---
+++ app/src/test/java/com/feryxz/jadwalimsyak1441h/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.feryxz.jadwalimsyak1441h
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
